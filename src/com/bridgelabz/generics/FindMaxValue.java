e0fe20a7d211722b9bad5e0cffdbc5bcf1d79673
namo-patil <namratapatil74738@gmail.com>
2022-05-19 02:49:12 +0530

    Added: Code to find maximum String

--- src/com/bridgelabz/generics/FindMaxValue.java
+++ src/com/bridgelabz/generics/FindMaxValue.java
@@ -20,11 +20,22 @@ public class FindMaxValue {
         if (number3.compareTo(maximumValue) > 0) {
             maximumValue = number3;
         }
-        System.out.println("Maximum Integer Value : " + maximumValue);
+        System.out.println("Maximum Float Value : " + maximumValue);
+    }
+    private static void maxStringValue(String str1, String str2, String str3) {
+        String maximumValue = str1;
+        if (str2.compareTo(maximumValue) > 0) {
+            maximumValue = str2;
+        }
+        if (str3.compareTo(maximumValue) > 0) {
+            maximumValue = str3;
+        }
+        System.out.println("Maximum String Value : " + maximumValue);
     }
 
     public static void main(String[] args) {
         maxIntegerValue(30, 60, 80);
         maxFloatValue(2.4f, 4.5f, 1.2f);
+        maxStringValue("Apple", "Peach", "Banana");
     }
 }
