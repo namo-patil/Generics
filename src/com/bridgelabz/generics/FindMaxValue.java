d8f54b1b4de05525a072ebc2874364a77f80dcca
namo-patil <namratapatil74738@gmail.com>
2022-05-19 02:27:46 +0530

    Added: Code to find maximum Float value

--- src/com/bridgelabz/generics/FindMaxValue.java
+++ src/com/bridgelabz/generics/FindMaxValue.java
@@ -1,8 +1,19 @@
 package com.bridgelabz.generics;
 
 public class FindMaxValue {
-    public static void maxIntegerValue(Integer number1, Integer number2, Integer number3) {
+    private static void maxIntegerValue(Integer number1, Integer number2, Integer number3) {
         Integer maximumValue = number1;
+        if (number2.compareTo(maximumValue) > 0) {
+            maximumValue = number2;
+        }
+        if (number3.compareTo(maximumValue) > 0) {
+            maximumValue = number3;
+        }
+        System.out.println("Maximum Integer Value : " + maximumValue);
+    }
+
+    private static void maxFloatValue(Float number1, Float number2, Float number3) {
+        Float maximumValue = number1;
         if (number2.compareTo(maximumValue) > 0) {
             maximumValue = number2;
         }
@@ -14,5 +25,6 @@ public class FindMaxValue {
 
     public static void main(String[] args) {
         maxIntegerValue(30, 60, 80);
+        maxFloatValue(2.4f, 4.5f, 1.2f);
     }
 }
