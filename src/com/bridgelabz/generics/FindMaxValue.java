a603d3befee3bf1d7fbca03e1e1b8da86a6b9c30
namo-patil <namratapatil74738@gmail.com>
2022-05-21 09:58:02 +0530

    Refactored: Converted all 3 to one generic method

--- src/com/bridgelabz/generics/FindMaxValue.java
+++ src/com/bridgelabz/generics/FindMaxValue.java
@@ -1,41 +1,23 @@
 package com.bridgelabz.generics;
 
 public class FindMaxValue {
-    private static void maxIntegerValue(Integer number1, Integer number2, Integer number3) {
-        Integer maximumValue = number1;
-        if (number2.compareTo(maximumValue) > 0) {
-            maximumValue = number2;
+    private static <T extends Comparable> void maxValue(T x, T y, T z) {
+        T maximumValue = x;
+        if (y.compareTo(maximumValue) > 0) {
+            maximumValue = y;
         }
-        if (number3.compareTo(maximumValue) > 0) {
-            maximumValue = number3;
+        if (z.compareTo(maximumValue) > 0) {
+            maximumValue = z;
         }
-        System.out.println("Maximum Integer Value : " + maximumValue);
-    }
-
-    private static void maxFloatValue(Float number1, Float number2, Float number3) {
-        Float maximumValue = number1;
-        if (number2.compareTo(maximumValue) > 0) {
-            maximumValue = number2;
-        }
-        if (number3.compareTo(maximumValue) > 0) {
-            maximumValue = number3;
-        }
-        System.out.println("Maximum Float Value : " + maximumValue);
-    }
-    private static void maxStringValue(String str1, String str2, String str3) {
-        String maximumValue = str1;
-        if (str2.compareTo(maximumValue) > 0) {
-            maximumValue = str2;
-        }
-        if (str3.compareTo(maximumValue) > 0) {
-            maximumValue = str3;
-        }
-        System.out.println("Maximum String Value : " + maximumValue);
+        System.out.println("Maximum Value is : " + maximumValue);
     }
 
     public static void main(String[] args) {
-        maxIntegerValue(30, 60, 80);
-        maxFloatValue(2.4f, 4.5f, 1.2f);
-        maxStringValue("Apple", "Peach", "Banana");
+        Integer x = 30, y = 60, z = 80;
+        Float a = 2.4f, b = 3.87f, c = 6.45f;
+        String str1 = "Apple", str2 = "Peach", str3 = "Banana";
+        maxValue(x, y, z);
+        maxValue(a, b, c);
+        maxValue(str1, str2, str3);
     }
 }
